4256b9bb5f7fc8ff574c01d1929a4cb80058dbb1
Shotaro Suzuki <shotaro@developeradvocate.jp>
2021-01-28 13:41:58 +0900

    first deploy

---
+++ api/GetData.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class GetData
+    {
+        [FunctionName("GetData")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            var results = new Data[] {
+                new Data { Id = 1, Name = "Shotaro" },
+                new Data { Id = 2, Name = "Suzuki" }
+             };
+
+            return new JsonResult(results);
+        }
+    }
+
+    public class Data {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+// namespace Company.Function
+// {
+//     public static class GetData
+//     {
+//         [FunctionName("GetData")]
+//         public static async Task<IActionResult> Run(
+//             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+//             ILogger log)
+//         {
+//             log.LogInformation("C# HTTP trigger function processed a request.");
+
+//             string name = req.Query["name"];
+
+//             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+//             dynamic data = JsonConvert.DeserializeObject(requestBody);
+//             name = name ?? data?.name;
+
+//             string responseMessage = string.IsNullOrEmpty(name)
+//                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+//                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+//             return new OkObjectResult(responseMessage);
+//         }
+//     }
+// }
---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+function App() {
+
+  const [response, setResponse] = React.useState<AxiosResponse>();
+
+  React.useEffect(() => {
+    //axios.get('http://localhost:7071/api/GetData')
+    axios.get('api/GetData') //Fixed 
+    .then(response => {
+      // handle success
+      setResponse(response);
+      console.log(response);
+    })
+    .catch(error => {
+      // handle error
+      console.log(error);
+    })
+    .then(() => {
+      // always executed
+    });
+    }, []);
+
+  return (
+    <React.Fragment>
+      {response?.data.map((elm:any, index:number) => {
+        return (
+          <div key={index}>id: {elm.id}, name: {elm.name}</div>
+        )
+      })}
+    </React.Fragment>
+  );
+}
+
+export default App;
